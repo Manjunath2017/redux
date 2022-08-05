60a96b39c1ce2270ca1314591e86eabc400cae89
Manjunath Jadhav <mja@durvah.com>
2022-08-05 11:34:54 +0530

    .

---
+++ src/Redux/fetchRecodAxios/fetchRecodAxiosReducer.js
@@ -0,0 +1,34 @@
+import { FETCH_USERS_FAILURE, FETCH_USERS_REQUEST, FETCH_USERS_SUCCESS } from "./fetchRecodAxiosType"
+
+const initialState={
+  loading:false,
+  users:[],
+  error:''
+}
+
+const FetchRecordAxiosReducer=(state=initialState,action)=>{
+  switch(action.type){
+    case FETCH_USERS_REQUEST:
+      return{
+        ...state,
+        loading:true
+      }
+    case FETCH_USERS_SUCCESS:
+      return{
+        ...state,
+        loading:false,
+        users:action.payload,
+        error:''
+      }
+    case FETCH_USERS_FAILURE:
+      return{
+        ...state,
+        loading:false,
+        users:[],
+        error:action.payload
+      }
+    default: return state
+  }
+}
+
+export default FetchRecordAxiosReducer
---
+++ src/component/fetchRecordAxios.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const fetchRecordAxios=()=> {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default fetchRecordAxios
