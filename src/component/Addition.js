b9ef02428f212eac9c456e83829e78163fe636d9
Manjunath Jadhav <mja@durvah.com>
2022-08-09 20:48:47 +0530

    hide and unhide

--- src/component/Addition.js
+++ src/component/Addition.js
@@ -1,14 +1,33 @@
-import React, { Fragment } from 'react'
+import React, { Fragment, useState, useEffect } from 'react'
 import { addOneFN, subOneFN } from '../Redux' 
 import {connect} from 'react-redux'
 import AdditionByFive from './AdditionByFive'
 
 const Addition =({number, addOneFN, subOneFN})=>{
+  const [display, setDispley]=useState(true)
+
+  const changeHeading=()=>{
+    console.log('display', display)
+    setDispley(!display)
+    console.log('display', display)
+  }
+
+  useEffect(() => {
+    console.log('changeHandle')
+  }, [changeHeading])  
   console.log('number', number)
   return(
   <Fragment >
     <div style={{textAlign:'center'}}>
-      <h1 >Addition and Substraction </h1> 
+    {display&&
+      (<h1 >Addition and Substraction </h1>) 
+    }
+    <h1 style={{display}}> Heading</h1>
+      <div>
+        <input type='button' onClick={changeHeading} value={display? 'hide Heading':'unhide Heading'} /> 
+      </div>
+
+      
       <input type='button' value='-' onClick={subOneFN} />
       <span> {number} </span>
       <input type='button' value='+' onClick={addOneFN} />
